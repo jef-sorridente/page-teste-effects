81c30b2773157f3680a22ecb221e906a8c3b8dc7
jeferson <jeferson@escape.ppg.br>
2025-01-24 16:49:31 -0300

    aeooo deu bom, agora falta o resto kkk

--- script.js
+++ script.js
@@ -1,73 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-//   gsap.registerPlugin(ScrollTrigger);
-//   gsap.to(".box", {
-//     scrollTrigger: {
-//       trigger: ".box",
-//       toggleActions: "restart pause reverse pause",
-//     },
-//     x: 500,
-//     duration: 1,
-//   });
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
 
-  const targetSection = document.getElementById("target-section");
-  const containerCards = document.getElementById("container-cards");
+  let items = gsap.utils.toArray(".items-cards"),
+    pageWrapper = document.querySelector(".page-wrapper");
 
-  window.addEventListener("scroll", () => {
-    // const sectionTop = targetSection.getBoundingClientRect().top;
-    const sectionBottom = targetSection.getBoundingClientRect().bottom;
-    const sectionHeight = targetSection.offsetHeight;
-    const windowHeight = window.innerHeight;
-
-    let progress = Math.min(
-      Math.max((windowHeight - sectionBottom) / sectionHeight, 0),
-      1
-    );
-
-    const translateX = 33.3 + progress * -33.3;
-    containerCards.style.transform = `translateX(${translateX}%)`;
-
-    const trasformValue = containerCards.style.transform;
-
-    if (trasformValue.includes("translateX")) {
-      const numericValue = parseInt(
-        trasformValue.match(/translateX\((-?\d+(\.\d+)?)%\)/)[1]
-      );
-
-      if (numericValue > 0 && sectionBottom < sectionHeight + 107) {
-        containerCards.classList.add("fixed");
-      } else {
-        containerCards.classList.remove("fixed");
-      }
-
-    }
+  items.forEach((container, i) => {
+    let localItems = container.querySelectorAll(".item"),
+      distance = () => {
+        let lastItemBounds =
+            localItems[localItems.length - 1].getBoundingClientRect(),
+          containerBounds = container.getBoundingClientRect();
+        return Math.max(0, lastItemBounds.right - containerBounds.right);
+      };
+    gsap.to(container, {
+      x: () => -distance(), // make sure it dynamically calculates things so that it adjusts to resizes
+      ease: "none",
+      scrollTrigger: {
+        trigger: container,
+        start: "top top",
+        pinnedContainer: pageWrapper,
+        end: () => "+=" + distance(),
+        pin: pageWrapper,
+        scrub: true,
+        invalidateOnRefresh: true, // will recalculate any function-based tween values on resize/refresh (making it responsive)
+      },
+    });
   });
-
-  //   window.addEventListener("scroll", () => {
-  //     const containerCards = document.getElementById("container-cards");
-  //     const triggerPoint = window.innerHeight / 2;
-
-  //     const scrollY = window.scrollY;
-
-  //     console.log(scrollY);
-
-  //     const containerTop = containerCards.getBoundingClientRect().top;
-
-  //     if (containerTop < triggerPoint) {
-  //         containerCards.style.transform = "translateX(0)";
-  //       containerCards.style.background = "red";
-  //     } else {
-  //       containerCards.style.background = "blue";
-  //       containerCards.style.transform = "translateX(33%)";
-  //     }
-
-  //     // if (containerTop < triggerPoint) {
-  //     //   containerCards.classList.add('fixed')
-  //     //   containerCards.style.transform = "translateX(0)";
-  //     //   containerCards.style.position = "fixed";
-  //     // } else {
-  //     //   // containerCards.classList.remove('fixed')
-  //     //   containerCards.style.transform = "translateX(33.3%)";
-  //     //   containerCards.style.position = "relative";
-  //     // }
-  //   });
 });
